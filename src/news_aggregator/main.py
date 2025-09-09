d00662ae486c4e039187995127e26fcca115a183
kseuro <kai.w.stewart@gmail.com>
2025-09-09 15:09:34 -0400

    implement two agent model for gathering the news and analyzing it.

--- src/news_aggregator/main.py
+++ src/news_aggregator/main.py
@@ -1,58 +1,89 @@
-# from agno.agent import Agent
-# from agno.models.google import Gemini
-# from agno.tools.duckduckgo import DuckDuckGoTools
-# from agno.storage.sqlite import SqliteStorage
-# from pydantic import BaseModel, Field
-
-import os
-import sys
+from textwrap import dedent
+from agno.agent import Agent
+from agno.models.google import Gemini
+from agno.tools.duckduckgo import DuckDuckGoTools
+from pydantic import BaseModel, Field
 from dotenv import dotenv_values
 from typing import List
-from src.news_aggregator.database import DataBase
-from src.news_aggregator.news import NewsAPI
 
-# TODO: Build off of this framework:
-# https://github.com/agno-agi/agno/blob/main/cookbook/workflows/hackernews_reporter.py
+
+class NewsStory(BaseModel):
+    author_name: str = Field(..., description="The name of a story's author or authors.")
+    publication_outlet: str = Field(..., description="Which outlet published the news story.")
+    story_url: str = Field(..., description="The URL of the story.")
+    summary: str = Field(..., description="A summary of the story along with key points.")
+
+
+class NewsStories(BaseModel):
+    stories: List[NewsStory] = Field(..., description="A collection of news stories.")
+
+
+class Analysis(BaseModel):
+    analysis: str = Field(..., description="Analysis of a collection of news stories.")
 
 
 if __name__ == "__main__":
-    which: str = "everything"  # TODO: headlines, both
-    keywords: List[str] | str = "nvidia"
-    language: str = "en"
-    sort_by: str = "relevancy"
-    n_articles: int | None = 10
+    n_articles: int | None = 2
 
     environment_config = dotenv_values(".env")
     GEMINI_API_KEY = environment_config.get("GEMINI_API_KEY", None)
-    NEWS_API_KEY = environment_config.get("NEWS_API_KEY", None)
+    if not GEMINI_API_KEY:
+        raise ValueError("Set the `GEMINI_API_KEY`")
+    keywords: List[str] = ["nvidia", "artificial intelligence", "business"]
+    model_id: str = "gemini-2.0-flash-lite"
 
-    if not NEWS_API_KEY:
-        NEWS_API_KEY = os.getenv("NEWS_API_KEY")
-        sys.exit("Please set the `NEWS_API_KEY`.")
+    # --- Gather the news ---
+    print(f"Gathering news related to: {' '.join(keywords)}")
+    news_gathering = Agent(
+        name="news_gathering",
+        model=Gemini(id=model_id, api_key=GEMINI_API_KEY),
+        tools=[DuckDuckGoTools(search=True, news=True)],
+        role="You are an experienced news gathering agent who scours the internet looking for the best stories.",
+        instructions=[
+            "Gather the following information:",
+            "Author Name",
+            "Publication Outlet",
+            "Story URL",
+            "A summary of the story along with key points",
+        ],
+        response_model=NewsStories,
+    )
+    n_stories = "the top story" if n_articles == 1 else f"{n_articles} stories"
+    prompt = dedent(
+        f"""
+    Search the web for {n_stories} related to the following keywords:
+    {', '.join(keywords)}
+    """
+    )
+    news_stories = news_gathering.run(prompt)
 
-    # TODO: Implement gemini checks (provider, model, etc)
-    # if not GEMINI_API_KEY:
-    #     sys.exit("Please set the `GEMINI_API_KEY`.")
-    # GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-    # model_id = "gemini-2.0-flash-lite"
+    # --- Analyze the news ---
+    print("Analyzing the news.")
+    news_analyst = Agent(
+        name="news_analyst",
+        model=Gemini(id=model_id, api_key=GEMINI_API_KEY),
+        role=dedent(
+            """
+        You are a seasoned information analyst who is deft at the task of synthesizing information
+        from multiple sources into a coherent narrative. A junior news gathering agent will provide you with summaries
+        of current news stories from which you'll generate the meta-analysis. Feel free to search the web for any
+        additional context or information that might enrich your analysis.
+        """
+        ),
+        tools=[DuckDuckGoTools(search=True)],
+        response_model=Analysis,
+    )
+    news_stories = "\n\n".join([story.summary for story in news_stories.content.stories])
+    news_prompt = dedent(
+        f"""
+    Review the information from the following news stories provided by the junior analyst and provide a
+    two paragraph analysis of the impact, implications, opportunities, drawbacks or downsides, and any future research
+    directions that might be spurred by what has been occurring in the news recently. If any of the preceeding analysis
+    topics are not relevant to the subject matter, then omit them from your analysis.
 
-    database = DataBase()
-    cached = database.get_cached_articles(which, keywords)
-    if cached is not None:
-        articles = cached
-        print(f"Retrieved {len(articles)} articles from cache for ({which},{keywords}) combination")
-    else:
-        newsapi = NewsAPI(api_key=NEWS_API_KEY, language=language)
-        if which == "everything" and keywords:  # take a look at just the news for these keywords
-            articles = newsapi.get_everything_by_keyword(keywords=keywords)
-            articles = articles[:n_articles]
-            database.set_cached_articles(which, keywords, articles)
-        elif which == "headlines":
-            # TODO: aggregate the top headlines and generate a report
-            print("`headlines` not yet implemented.")
-        elif which == "all":
-            print("`all` aggregation not yet implemented.")
-            # TODO: Generate a report about the top stories related to the provided keywords.
-            # TODO: Generate a report about the top news stories happening right now.
-
-    # TODO: Agent workflow here
+    News Stories:
+    {news_stories}
+    """
+    )
+    analysis = news_analyst.run(news_prompt)
+    print(analysis.content)
